c3b871180ed4b3ef2c119ba7064d583d12f8bc9b
Nick Foden <nickfoden@gmail.com>
2025-02-12 13:28:04 -0500

    Add some posts about bugs

--- internal/content/posts.go
+++ internal/content/posts.go
@@ -12,6 +12,16 @@ var posts = []models.Post{
 		ContentPreview: "A \"simple\" web log built with Go and html + htmx.",
 		CreatedAt:      time.Date(2025, 1, 3, 0, 0, 0, 0, time.UTC),
 		Slug:           "1"},
+	{Title: "No Bugs Here",
+		Content:        "",
+		ContentPreview: "C'mon just use a proper tsconfig",
+		CreatedAt:      time.Date(2025, 1, 15, 0, 0, 0, 0, time.UTC),
+		Slug:           "no-bugs"},
+	{Title: "Ruby for React Native",
+		Content:        "",
+		ContentPreview: "RN setup issues? Ensure Ruby is current",
+		CreatedAt:      time.Date(2025, 1, 16, 0, 0, 0, 0, time.UTC),
+		Slug:           "react-native-requires-current-ruby"},
 }
 
 func GetAllPosts() []models.Post {
